f5d1acbe49bfdc0dfcfa9274c5e57395eea797f5
XhuuLL <fatkhularif15@gmail.com>
2025-12-26 21:33:15 +0700

    Fix: Final clean up for deployment

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Variabel database Supabase Anda
+
 const supabaseUrl = 'https://rzzfaalrjojhlvjddvzl.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6emZhYWxyam9qaGx2amRkdnpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MDg1MDksImV4cCI6MjA3OTM4NDUwOX0.3mqw3in19X5W5VU_YMM6LE_gbS9IqZJQzxvH_nj6O7w' 
 
