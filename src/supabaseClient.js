28e6861c09fa33cf607b60cc6c080f83ae72374e
XhuuLL <fatkhularif15@gmail.com>
2025-12-26 21:31:18 +0700

    Fix: Membersihkan sisa tanda merge conflict

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,12 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> d9a2b833e3ebae687079c2c5f214b5091f83403e
+// Variabel database Supabase Anda
 const supabaseUrl = 'https://rzzfaalrjojhlvjddvzl.supabase.co'
-
-
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6emZhYWxyam9qaGx2amRkdnpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MDg1MDksImV4cCI6MjA3OTM4NDUwOX0.3mqw3in19X5W5VU_YMM6LE_gbS9IqZJQzxvH_nj6O7w' 
 
+// Inisialisasi client
 export const supabase = createClient(supabaseUrl, supabaseKey)
